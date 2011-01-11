3204ee6d6baeaabf6c4098d2617ef4863c70e7ff
Ben Charrow <bcharrow@seas.upenn.edu>
2011-01-11 14:22:36 -0500

    Uncommented python include and commented out flymake/autocomplete stuff

--- bcharrow-python.el
+++ bcharrow-python.el
@@ -13,8 +13,7 @@
                          "~/.emacs.d/ropemacs/ropemacs"))
 
 (add-hook 'python-mode-hook
-          (lambda()
-            (define-key python-mode-map "\r" 'newline-and-indent)))
+          (define-key python-mode-map "\r" 'newline-and-indent))
 
 ;================================= DEBUGGING =================================;
 ;;http://www.emacswiki.org/emacs/PdbNotes
@@ -58,84 +57,84 @@
 ;; (add-hook 'python-mode-hook 'load-ysnippet)
 
 ;============================== AUTOCOMPLETION ===============================;
-(defun prefix-list-elements (list prefix)
-  (let (value)
-    (nreverse
-     (dolist (element list value)
-      (setq value (cons (format "%s%s" prefix element) value))))))
-(defvar ac-source-rope
-  '((candidates
-     . (lambda ()
-         (prefix-list-elements (rope-completions) ac-target))))
-  "Source for Rope")
-(defun ac-python-find ()
-  "Python `ac-find-function'."
-  (require 'thingatpt)
-  (let ((symbol (car-safe (bounds-of-thing-at-point 'symbol))))
-    (if (null symbol)
-        (if (string= "." (buffer-substring (- (point) 1) (point)))
-            (point)
-          nil)
-      symbol)))
-(defun ac-python-candidate ()
-  "Python `ac-candidates-function'"
-  (let (candidates)
-    (dolist (source ac-sources)
-      (if (symbolp source)
-          (setq source (symbol-value source)))
-      (let* ((ac-limit (or (cdr-safe (assq 'limit source)) ac-limit))
-             (requires (cdr-safe (assq 'requires source)))
-             cand)
-        (if (or (null requires)
-                (>= (length ac-target) requires))
-            (setq cand
-                  (delq nil
-                        (mapcar (lambda (candidate)
-                                  (propertize candidate 'source source))
-                                (funcall (cdr (assq 'candidates source)))))))
-        (if (and (> ac-limit 1)
-                 (> (length cand) ac-limit))
-            (setcdr (nthcdr (1- ac-limit) cand) nil))
-        (setq candidates (append candidates cand))))
-    (delete-dups candidates)))
-(add-hook 'python-mode-hook
-          (lambda ()
-                 (auto-complete-mode 1)
-                 (set (make-local-variable 'ac-sources)
-                      (append ac-sources '(ac-source-yasnippet)  '(ac-source-rope)))
-                 (set (make-local-variable 'ac-find-function) 'ac-python-find)
-                 (set (make-local-variable 'ac-candidate-function) 'ac-python-candidate)
-                 (set (make-local-variable 'ac-auto-start) nil)))
+;; (defun prefix-list-elements (list prefix)
+;;   (let (value)
+;;     (nreverse
+;;      (dolist (element list value)
+;;       (setq value (cons (format "%s%s" prefix element) value))))))
+;; (defvar ac-source-rope
+;;   '((candidates
+;;      . (lambda ()
+;;          (prefix-list-elements (rope-completions) ac-target))))
+;;   "Source for Rope")
+;; (defun ac-python-find ()
+;;   "Python `ac-find-function'."
+;;   (require 'thingatpt)
+;;   (let ((symbol (car-safe (bounds-of-thing-at-point 'symbol))))
+;;     (if (null symbol)
+;;         (if (string= "." (buffer-substring (- (point) 1) (point)))
+;;             (point)
+;;           nil)
+;;       symbol)))
+;; (defun ac-python-candidate ()
+;;   "Python `ac-candidates-function'"
+;;   (let (candidates)
+;;     (dolist (source ac-sources)
+;;       (if (symbolp source)
+;;           (setq source (symbol-value source)))
+;;       (let* ((ac-limit (or (cdr-safe (assq 'limit source)) ac-limit))
+;;              (requires (cdr-safe (assq 'requires source)))
+;;              cand)
+;;         (if (or (null requires)
+;;                 (>= (length ac-target) requires))
+;;             (setq cand
+;;                   (delq nil
+;;                         (mapcar (lambda (candidate)
+;;                                   (propertize candidate 'source source))
+;;                                 (funcall (cdr (assq 'candidates source)))))))
+;;         (if (and (> ac-limit 1)
+;;                  (> (length cand) ac-limit))
+;;             (setcdr (nthcdr (1- ac-limit) cand) nil))
+;;         (setq candidates (append candidates cand))))
+;;     (delete-dups candidates)))
+;; (add-hook 'python-mode-hook
+;;           (lambda ()
+;;                  (auto-complete-mode 1)
+;;                  (set (make-local-variable 'ac-sources)
+;;                       (append ac-sources '(ac-source-yasnippet)  '(ac-source-rope)))
+;;                  (set (make-local-variable 'ac-find-function) 'ac-python-find)
+;;                  (set (make-local-variable 'ac-candidate-function) 'ac-python-candidate)
+;;                  (set (make-local-variable 'ac-auto-start) nil)))
 
-(defadvice ac-start (before advice-turn-on-auto-start activate)
-  (set (make-local-variable 'ac-auto-start) t))
-(defadvice ac-cleanup (after advice-turn-off-auto-start activate)
-  (set (make-local-variable 'ac-auto-start) nil))
+;; (defadvice ac-start (before advice-turn-on-auto-start activate)
+;;   (set (make-local-variable 'ac-auto-start) t))
+;; (defadvice ac-cleanup (after advice-turn-off-auto-start activate)
+;;   (set (make-local-variable 'ac-auto-start) nil))
 
-; Try the following:
-; 1) Do a yasnippet expansion
-; 2) Do a Rope code completion
-(define-key python-mode-map [(ctrl .)] 'ac-start)
+;; ; Try the following:
+;; ; 1) Do a yasnippet expansion
+;; ; 2) Do a Rope code completion
+;; (define-key python-mode-map [(ctrl .)] 'ac-start)
 
-;================================== FLYMAKE ==================================;
-; Snagged from http://www.plope.com/Members/chrism/flymake-mode
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
+;; ;================================== FLYMAKE ==================================;
+;; ; Snagged from http://www.plope.com/Members/chrism/flymake-mode
+;; (when (load "flymake" t)
+;;   (defun flymake-pyflakes-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       (list "pyflakes" (list local-file))))
 
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-;; Only use flymake for python
-(add-hook 'python-mode-hook 'flymake-find-file-hook)
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pyflakes-init)))
+;; ;; Only use flymake for python
+;; (add-hook 'python-mode-hook 'flymake-find-file-hook)
 
-(custom-set-faces
- '(flymake-errline ((((class color)) (:background "LightPink" :foreground "black"))))
- '(flymake-warnline ((((class color)) (:background "LightBlue2" :foreground "black")))))
+;; (custom-set-faces
+;;  '(flymake-errline ((((class color)) (:background "LightPink" :foreground "black"))))
+;;  '(flymake-warnline ((((class color)) (:background "LightBlue2" :foreground "black")))))
 
 
 ;================================== COMPILE ==================================;
